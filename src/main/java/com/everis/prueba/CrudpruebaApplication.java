19df6bcd15b62ec66e9b2c9abca2497bc8ea45ce
Miguel Angel Vega Alonso <mivegaal@everis.com>
2020-12-18 08:54:14 -0500

    Prueba

---
+++ src/main/java/com/everis/prueba/CrudpruebaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpruebaApplication.class, args);
+	}
+
+}
